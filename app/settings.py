30cb38f32643571da1fed245f42c4ce1516ae92d
Fernando Quisaguano <ferjeff92@gmail.com>
2024-08-13 12:22:16 -0500

    una nueva api

---
+++ app/settings.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    debug: bool = False
+    environment: str
+    database_url: str
+    model_config = SettingsConfigDict(env_file=".env")
